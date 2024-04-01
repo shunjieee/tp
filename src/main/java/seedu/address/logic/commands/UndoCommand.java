fafac7ed5bd0291ade16d1698e1404db57245923
LIU JIAYAO <e1092092@u.nus.edu>
2024-04-01 09:15:35 +0800

    Create UndoCommand class

---
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -0,0 +1,32 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Represents a command with the ability to undo the last executed command.
+ * When executed, the command will attempt to revert the address book to the previous state.
+ */
+public class UndoCommand extends Command {
+    public static final String COMMAND_WORD = "undo";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Undo the last executed command. ";
+    public static final String MESSAGE_UNDO_SUCCESS = "The command: %1$s has been undone.";
+    public static final String MESSAGE_UNDO_FAILURE = "There is no more command to undo!";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        if (!model.canUndoAddressBook()) {
+            throw new CommandException(MESSAGE_UNDO_FAILURE);
+        }
+
+        model.undoAddressBook();
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_UNDO_SUCCESS);
+    }
+
+}
